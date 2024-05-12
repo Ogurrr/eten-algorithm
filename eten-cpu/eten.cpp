1e3ccfac149a6e2f24409fa3d12a13c645dfd3d0
Aleksander Plominski <plominski.aleksander@gmail.com>
2024-05-12 19:14:47 +0200

    update

--- eten-cpu/eten.cpp
+++ eten-cpu/eten.cpp
@@ -76,7 +76,7 @@ std::string etenCalc(std::string hash,std::string poolKey,int difficult)
     hash = bitwise_xor(hash,poolKey);
     hash = bitwise_or(hash,poolKey);
     hash = bitwise_and(hash,poolKey);
-       hash = bitwise_nand(hash,poolKey);
+    hash = bitwise_nand(hash,poolKey);
     for(int i; i < difficult; i++)
     {
         hash = bitwise_xor(hash,poolKey);
@@ -125,6 +125,47 @@ std::string etenCalc(std::string hash,std::string poolKey,int difficult)
     hash = sha512(hash);
     poolKey = sha512(poolKey);
 
+    poolKey = encode(poolKeyBinarySplit);
+    hash = encode(hashBinarySplit);
+    hash = sha256(hash);
+    poolKey = sha256(poolKey);
+    hash = MD5(hash);
+    poolKey = MD5(poolKey);
+
+    hash = bitwise_xor(hash,poolKey);
+    hash = bitwise_or(hash,poolKey);
+    hash = bitwise_and(hash,poolKey);
+    hash = bitwise_nand(hash,poolKey);
+    hash = sha512(hash);
+    poolKey = sha512(poolKey);
+
+    hash = bitwise_and(hash,poolKey);
+    hash = bitwise_nand(hash,poolKey);
+    for(int i; i < difficult ^ 5; i++)
+    {
+        hash = bitwise_xor(hash,poolKey);
+        hash = bitwise_or(hash,poolKey);
+        hash = bitwise_and(hash,poolKey);
+        
+        hash = toBinaryString(hash);
+        poolKey = toBinaryString(poolKey);
+        poolKeyBinarySplit = splitBinary(poolKey);
+        
+        poolKey = encode(poolKeyBinarySplit);
+        hash = encode(hashBinarySplit);
+
+        hash = sha512(hash);
+        poolKey = sha512(poolKey);
+
+        hash = sha256(hash);
+        poolKey = sha256(poolKey);
+        hash = MD5(hash);
+        poolKey = MD5(poolKey);
+        hash = sha512(hash);
+        poolKey = sha512(poolKey);
+        poolKey = encryptRC4(poolKey,hash);
+        hash = encryptRC4(hash,poolKey);
+    }
     poolKey = encode(poolKeyBinarySplit);
     hash = encode(hashBinarySplit);
     hash = sha256(hash);
