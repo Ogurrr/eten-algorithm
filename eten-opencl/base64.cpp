ce25e3a8275f1a1b1a1d39ef6c435c5086881858
Aleksander Plominski <plominski.aleksander@gmail.com>
2024-05-17 22:32:54 +0000

    base64 updates

--- eten-opencl/base64.cpp
+++ eten-opencl/base64.cpp
@@ -1,14 +1,15 @@
+
+Przepraszam za niedokładność. Poprawiłem to i teraz kod zwraca zaszyfrowany ciąg jako wynik funkcji encode. Nie wypisuje on niczego bezpośrednio na standardowe wyjście. Jeśli chcesz wyświetlić zaszyfrowany ciąg, możesz to zrobić w funkcji main po wywołaniu funkcji encode. Oto poprawiony kod:
+
+cpp
+Skopiuj kod
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 #include <CL/cl.h>
 #include <cstring>
-#define CHECK_ERROR(err) if (err != CL_SUCCESS) { printf("Error: %d\n", err); exit(EXIT_FAILURE); }
 
 const char* Base64kernelSource = R"(
-__constant char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
 __kernel void base64_encode(__global const unsigned char* input, __global char* output, int input_size) {
+    const char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     int gid = get_global_id(0);
 
     if (gid * 3 < input_size) {
@@ -24,12 +25,12 @@ __kernel void base64_encode(__global const unsigned char* input, __global char* 
 }
 )";
 
-std::string encode(std::string inputed) {
+std::string encode(const std::string& inputed) {
     const char* input = inputed.c_str();
     size_t input_size = strlen(input);
     size_t output_size = ((input_size + 2) / 3) * 4;
     
-    char* output = (char*)malloc(output_size + 1);
+    char* output = new char[output_size + 1];
     output[output_size] = '\0';
 
     cl_int err;
@@ -40,37 +41,142 @@ std::string encode(std::string inputed) {
     cl_program program;
     cl_kernel kernel;
 
-    err = clGetPlatformIDs(1, &platform, NULL); CHECK_ERROR(err);
-    err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL); CHECK_ERROR(err);
+    err = clGetPlatformIDs(1, &platform, NULL); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error getting platform IDs: " << err << std::endl;
+        delete[] output;
+        return "";
+    }
+    err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error getting device IDs: " << err << std::endl;
+        delete[] output;
+        return "";
+    }
 
-    context = clCreateContext(NULL, 1, &device, NULL, NULL, &err); CHECK_ERROR(err);
-    queue = clCreateCommandQueue(context, device, 0, &err); CHECK_ERROR(err);
+    context = clCreateContext(NULL, 1, &device, NULL, NULL, &err); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error creating context: " << err << std::endl;
+        delete[] output;
+        return "";
+    }
+    queue = clCreateCommandQueue(context, device, 0, &err); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error creating command queue: " << err << std::endl;
+        delete[] output;
+        return "";
+    }
 
-    program = clCreateProgramWithSource(context, 1, &Base64kernelSource, NULL, &err); CHECK_ERROR(err);
-    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL); CHECK_ERROR(err);
+    program = clCreateProgramWithSource(context, 1, &Base64kernelSource, NULL, &err); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error creating program with source: " << err << std::endl;
+        delete[] output;
+        return "";
+    }
+    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error building program: " << err << std::endl;
+        size_t len;
+        char buffer[2048];
+        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
+        std::cerr << "Build log: " << buffer << std::endl;
+        clReleaseProgram(program);
+        delete[] output;
+        return "";
+    }
 
-    kernel = clCreateKernel(program, "base64_encode", &err); CHECK_ERROR(err);
+    kernel = clCreateKernel(program, "base64_encode", &err); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error creating kernel: " << err << std::endl;
+        clReleaseProgram(program);
+        delete[] output;
+        return "";
+    }
 
-    cl_mem input_buffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, input_size, (void*)input, &err); CHECK_ERROR(err);
-    cl_mem output_buffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, output_size, NULL, &err); CHECK_ERROR(err);
+    cl_mem input_buffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, input_size, (void*)input, &err); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error creating input buffer: " << err << std::endl;
+        clReleaseKernel(kernel);
+        clReleaseProgram(program);
+        delete[] output;
+        return "";
+    }
+    cl_mem output_buffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, output_size, NULL, &err); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error creating output buffer: " << err << std::endl;
+        clReleaseMemObject(input_buffer);
+        clReleaseKernel(kernel);
+        clReleaseProgram(program);
+        delete[] output;
+        return "";
+    }
 
-    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &input_buffer); CHECK_ERROR(err);
-    err = clSetKernelArg(kernel, 1, sizeof(cl_mem), &output_buffer); CHECK_ERROR(err);
-    err = clSetKernelArg(kernel, 2, sizeof(int), &input_size); CHECK_ERROR(err);
+    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &input_buffer); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error setting kernel arg 0: " << err << std::endl;
+        clReleaseMemObject(input_buffer);
+        clReleaseMemObject(output_buffer);
+        clReleaseKernel(kernel);
+        clReleaseProgram(program);
+        delete[] output;
+        return "";
+    }
+    err = clSetKernelArg(kernel, 1, sizeof(cl_mem), &output_buffer); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error setting kernel arg 1: " << err << std::endl;
+        clReleaseMemObject(input_buffer);
+        clReleaseMemObject(output_buffer);
+        clReleaseKernel(kernel);
+        clReleaseProgram(program);
+        delete[] output;
+        return "";
+    }
+    err = clSetKernelArg(kernel, 2, sizeof(int), &input_size); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error setting kernel arg 2: " << err << std::endl;
+        clReleaseMemObject(input_buffer);
+        clReleaseMemObject(output_buffer);
+        clReleaseKernel(kernel);
+        clReleaseProgram(program);
+        delete[] output;
+        return "";
+    }
 
     size_t global_size = (input_size + 2) / 3;
 
-    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global_size, NULL, 0, NULL, NULL); CHECK_ERROR(err);
+    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global_size, NULL, 0, NULL, NULL);
 
+Przepraszam za przerwanie, wygląda na to, że popełniłem błąd. Kontynuujmy przepisywanie reszty kodu:
 
-    err = clEnqueueReadBuffer(queue, output_buffer, CL_TRUE, 0, output_size, output, 0, NULL, NULL); CHECK_ERROR(err);
+cpp
+Skopiuj kod
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error enqueuing ND range kernel: " << err << std::endl;
+        clReleaseMemObject(input_buffer);
+        clReleaseMemObject(output_buffer);
+        clReleaseKernel(kernel);
+        clReleaseProgram(program);
+        delete[] output;
+        return "";
+    }
+
+    err = clEnqueueReadBuffer(queue, output_buffer, CL_TRUE, 0, output_size, output, 0, NULL, NULL); 
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error reading output buffer: " << err << std::endl;
+        clReleaseMemObject(input_buffer);
+        clReleaseMemObject(output_buffer);
+        clReleaseKernel(kernel);
+        clReleaseProgram(program);
+        delete[] output;
+        return "";
+    }
 
     clReleaseMemObject(input_buffer);
     clReleaseMemObject(output_buffer);
     clReleaseKernel(kernel);
     clReleaseProgram(program);
-    clReleaseCommandQueue(queue);
-    clReleaseContext(context);
 
-    return output;
+    std::string result(output);
+    delete[] output;
+    return result;
 }
