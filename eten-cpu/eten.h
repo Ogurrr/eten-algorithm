f00a68087f5a790e56b50d440dd8f244a8440db1
Aleksander Plominski <plominski.aleksander@gmail.com>
2024-08-13 17:37:28 +0200

    bug fixes

--- eten-cpu/eten.h
+++ eten-cpu/eten.h
@@ -1,4 +1,6 @@
 #include <iostream>
 
 std::string etenCompute(std::string hash,std::string poolKey,int difficult);
-std::string etenCalc(std::string hash, std::string poolKey, int difficult,int size);
+extern "C" {
+    std::string etenCalc(std::string hash, std::string poolKey, int difficult,int size);
+}
